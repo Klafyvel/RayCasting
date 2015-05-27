b5484090832d3bb1b38fe0caef23c13e38c3b690
Klafyvel <klafyvel@gmail.com>
2015-05-27 16:19:19 +0200

    Début du calcul des hauteurs de colonnes.

--- blocks.py
+++ blocks.py
@@ -1,28 +1,33 @@
 #! /usr/bin/python3
 
+BLOCK_WIDTH = 100
+BLOCK_HEIGHT = 100
+BLOCK_THICKNESS = 100
 
 EMPTY = (0,0)
 wall = lambda c : (1,c) 
 
-class Block:
-    """A block is an ampty space."""
-    def __init__(self, pos):
-        self.x, self.y = pos
-    def on_render(self, target):
-        return EMPTY
+# Later ;)
 
-class Wall(Block):
-    """A wall is a block which is not empty :p"""
-    def __init__(self, pos, color):
-        super().__init__(pos)
-        self.color = color:
-    def on_render(self, target):
-        return wall(self.color)
+# class Block:
+#     """A block is an ampty space."""
+#     def __init__(self, pos):
+#         self.x, self.y = pos
+#     def on_render(self, target):
+#         return EMPTY
 
-class Map:
-    """A map contains blocks and walls."""
-    def __init__(self):
-        self.map = []
-    def get_block(self, pos):
-        x,y = pos
-        return self.map[y][x]
+# class Wall(Block):
+#     """A wall is a block which is not empty :p"""
+#     def __init__(self, pos, color):
+#         super().__init__(pos)
+#         self.color = color:
+#     def on_render(self, target):
+#         return wall(self.color)
+
+# class Map:
+#     """A map contains blocks and walls."""
+#     def __init__(self):
+#         self.map = []
+#     def get_block(self, pos):
+#         x,y = pos
+#         return self.map[y][x]
--- ray_cast_engine.py
+++ ray_cast_engine.py
@@ -1,5 +1,47 @@
 #! /usr/bin/python3
 
+from math import sqrt
+
+from blocks import *
+
+norm = lambda x, y : sqrt(x**2 + y**2)
+
 class RayCastEngine:
-    def __init__(self):
-        pass
+    def __init__(self, player, world, screen_size=(0,0)):
+        self.world = world
+        self.screen_width, self.screen_height = screen_size
+        self.player = player
+    def get_obstacle_in_direction(self, first_point, second_point):
+        x1, y1 = first_point
+        x2, y2 = second_point
+
+        f = lambda t: ((x2 - x1) * t + x1, (y2 - y1) * t + y1)
+
+        obstacle = False
+        out_of_map = False
+        t = 1 # f(1) = (x2, y2)
+        step = 1 / norm(x2-x1, y2 - y1)
+
+        while not obstacle and not out_of_map:
+            x,y = f(t)
+            i,j = x / BLOCK_WIDTH, y / BLOCK_HEIGHT
+            if j >= len(self.world) or i >= len(self.world[j]):
+                out_of_map = True
+            elif self.world[j][i][0] is 1:
+                obstacle = True
+            t += step
+        if out_of_map:
+            return None
+        else:
+            return f(t)
+    def calc_column_height(self, pos_column):
+        x, y = self.player.pos
+        x_col,y_co: = pos_column
+        return norm(*self.player.camera_vector) / norm(x-x_col, y-y_col) * BLOCK_THICKNESS
+
+
+
+class Player:
+    def __init__(self, screen_size=(0,0), pos=(0,0)):
+        self.screen_size = screen_size
+        self.x, self.y = pos
